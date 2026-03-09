f588a62377c9e56b14a3c120db24fdd7826f80e3
jm-melchioretto09w <joao_melchioretto150@estudante.sesisenai.org.br>
2026-03-09 16:02:44 -0300

    Commit 4 - reconhecimento de resistores
    Reconhecimento de resistores implementado por completo, lendo resistores de 4 e 5 faixas realizando o cálculo corretamente

--- reconhecerResistores.js
+++ reconhecerResistores.js
@@ -1,23 +1,55 @@
 function descobrirResist(){
     const tabelaDigito = {
-        'Preto' : 0,
-        'Marrom' : 1,
-        'Vermleho' : 2,
-        'Laranja' : 3,
-        'Amarelo' : 4,
-        'Verde' : 5,
-        'Azul' : 6,
-        'Violeta' : 7,
-        'Cinza' : 8,
-        'Branco' : 9  
+        'preto' : '0',
+        'marrom' : '1',
+        'vermelho' : '2',
+        'laranja' : '3',
+        'amarelo' : '4',
+        'verde' : '5',
+        'azul' : '6',
+        'violeta' : '7',
+        'cinza' : '8',
+        'branco' : '9'  
     };
-    //fazer uma tabela para o multiplicador
+    const tabelaMultiplo = {
+        'preto' : 10 ** 0,
+        'marrom' : 10 ** 1,
+        'vermelho' : 10 ** 2,
+        'laranja' : 10 ** 3,
+        'amarelo' : 10 ** 4,
+        'verde' : 10 ** 5,
+        'azul' : 10 ** 6,
+        'violeta' : 10 ** 7,
+        'cinza' : 10 ** 8,
+        'branco' : 10 ** 9,
+        'ouro' : 10 ** -1,
+        'prata' : 10 ** -2 
+    };
+    const tabelaTolerar = {
+        'marrom' : 1,
+        'vermelho' : 2,
+        'ouro' : 5,
+        'prata' : 10,
+        'branco' : 20
+    }
     //fazer uma tabela de tolerância
-    let faixaNum = parseInt('Insira a uantidade de faixas(4 ou 5)');
+    let faixaNum = parseInt(prompt('Insira a uantidade de faixas(4 ou 5)'));
     let value = '';
 
     for(let i = 0; i < faixaNum; i++){
         let color = prompt('Insira a cor da ' + (i + 1) + 'ª faixa');
-       console.log(tabelaDigito[color]);
+        color = color.toLowerCase();
+
+        if(color == 'dourado'){
+            color = 'ouro';
+        }
+
+        if(i == faixaNum - 1){
+            alert('O resistor terá uma resistência de ' + value + ' Ohms com tolerância de ' + tabelaTolerar[color] + '%, que seriam, aproximadamente, ' + (value * (tabelaTolerar[color]/100)).toFixed(2) + ' Ohms a mais ou a menos');
+        }else if(i == faixaNum - 2){
+            value = Number(value) * tabelaMultiplo[color];
+        }else{
+            value += tabelaDigito[color];
+        }
     }
 }
